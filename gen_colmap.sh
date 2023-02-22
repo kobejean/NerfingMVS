ecc5a9bc997d4827e5406d93c84f474bba70137c
kobejean <s1292010@u-aizu.ac.jp>
2023-02-22 21:12:26 +0900

    update

---
+++ gen_colmap.sh
@@ -0,0 +1,7 @@
+sh colmap.sh data/scene0079_00
+sh colmap.sh data/scene0158_00
+sh colmap.sh data/scene0316_00
+sh colmap.sh data/scene0521_00
+sh colmap.sh data/scene0553_00
+sh colmap.sh data/scene0616_00
+sh colmap.sh data/scene0653_00
